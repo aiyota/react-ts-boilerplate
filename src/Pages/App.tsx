6f40b49516d362c6c71421e5fb50249a2b2cef4a
Aaron Yota <aiyota@outlook.com>
2022-05-16 14:20:04 -0700

    Add example component, 'Container'.

--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import BodyParagraph from "../Components/BodyParagraph";
+import Container from "../Components/Container";
 import Heading from "../Components/Heading";
 
 const App = () => (
-  <>
+  <Container>
     <Heading text={"My React TS App"} />
 
     <BodyParagraph
@@ -11,7 +12,7 @@ const App = () => (
         amet consectetur adipisicing elit. Iusto veritatis autem dignissimos nam aut est? 
         Dolorem optio maxime aut tempore.`}
     />
-  </>
+  </Container>
 );
 
 export default App;
